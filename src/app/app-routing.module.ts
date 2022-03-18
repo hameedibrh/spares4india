7516cba2f1369647dc8a708ecc9674a5a6ec2a4d
Hameed Ibrahim <hameedibrh@gmail.com>
2022-03-18 19:36:18 +0530

    First built

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
